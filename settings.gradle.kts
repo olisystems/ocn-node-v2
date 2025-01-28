c57d18c597f957b19ab380fb4f19e45c60116ff2
Matheus Rosendo <matheus.rosendo@my-oli.com>
2025-01-27 21:42:10 -0300

    feat: included banula-prod option in jenkins file

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,3 +4,8 @@ pluginManagement {
 	}
 }
 rootProject.name = "ocn-node"
+
+// used in local dev only and overriden in CI
+gradle.beforeProject {
+	project.extensions.extraProperties["profile"] = "local-minikube"
+}
