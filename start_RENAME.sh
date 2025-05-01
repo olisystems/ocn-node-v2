1d74deedaa37b2f88adfd9451fb9bb231919d0a4
Matheus Rosendo <matheus.rosendo@my-oli.com>
2025-05-01 11:01:48 -0300

    feat: included start.sh in gitignore

--- start_RENAME.sh
+++ start_RENAME.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+# Rename to start.sh and update the line above to your local Java path
 JAVA_HOME="/Users/matheusrosendo/Library/Java/JavaVirtualMachines/azul-13.0.14/Contents/Home" ./gradlew bootRun -Pprofile=local-minikube -Dorg.gradle.jvmargs='--add-opens=jdk.compiler/com.sun.tools.javac.main=ALL-UNNAMED --add-opens=jdk.compiler/com.sun.tools.javac.processing=ALL-UNNAMED --add-opens=jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED'
