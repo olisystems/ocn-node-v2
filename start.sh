0b56367ae22146979f457da89f193c6725847d11
EltonSaraci99 <97944829+EltonSaraci99@users.noreply.github.com>
2025-05-06 14:32:01 +0200

    Delete start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Start the application using the verified working command from project memory
-JAVA_HOME="/Users/matheusrosendo/Library/Java/JavaVirtualMachines/azul-13.0.14/Contents/Home" \
-./gradlew bootRun -Pprofile=local-minikube \
--Dorg.gradle.jvmargs='--add-opens=jdk.compiler/com.sun.tools.javac.main=ALL-UNNAMED --add-opens=jdk.compiler/com.sun.tools.javac.processing=ALL-UNNAMED --add-opens=jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED'
