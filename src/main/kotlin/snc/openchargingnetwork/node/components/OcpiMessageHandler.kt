9bc97114b15b6fdef58ca873e5fb0d6f617aae2c
Matheus Rosendo <matheus.rosendo@my-oli.com>
2025-07-11 20:52:51 -0300

    fix: assure no signatures is going to be verified if signatures variable is off

--- src/main/kotlin/snc/openchargingnetwork/node/components/OcpiMessageHandler.kt
+++ src/main/kotlin/snc/openchargingnetwork/node/components/OcpiMessageHandler.kt
@@ -50,11 +50,15 @@ open class OcpiMessageHandler(
      */
     fun isSigningActive(recipient: BasicRole? = null): Boolean {
         var active = properties.signatures || request.headers.signature != null
-        if (recipient != null) {
-            val recipientRules = routingService.getPlatformRules(recipient)
-            active = active || recipientRules.signatures
-        }
-        return active
+       if(active) {
+           if (recipient != null) {
+               val recipientRules = routingService.getPlatformRules(recipient)
+               active = active || recipientRules.signatures
+           }
+           return active
+       } else {
+           return false
+       }
     }
 
     /**
