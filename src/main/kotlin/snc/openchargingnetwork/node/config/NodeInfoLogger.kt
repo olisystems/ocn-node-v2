cf940a9e68cb539ad62485b2e213630601478e1a
Diego Rosales <diego.rosales@my-oli.com>
2025-11-19 00:07:32 -0600

    feat: added on start log

--- src/main/kotlin/snc/openchargingnetwork/node/config/NodeInfoLogger.kt
+++ src/main/kotlin/snc/openchargingnetwork/node/config/NodeInfoLogger.kt
@@ -49,8 +49,8 @@ class NodeInfoLogger(
                 "\n${border.substring(0, 3)} NODE INFO ${border.substring(17)}\n" +
                         " URL     | ${properties.url}/${properties.apiPrefix}\n" +
                         " ADDRESS | $addressText\n" +
-                        " API KEY | ${properties.apikey}" +
-                        " COUNTRY CODE | ${properties.countryCode}" +
+                        " API KEY | ${properties.apikey}\n" +
+                        " COUNTRY CODE | ${properties.countryCode}\n" +
                         " PARTY ID     | ${properties.partyId}"
         )
 
