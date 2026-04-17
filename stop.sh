e06b3ab6a5a191afa7fd374380487345113e41ef
Matheus Rosendo <matheus.rosendo@my-oli.com>
2026-04-17 10:09:07 -0300

    refactor: update stop script

--- stop.sh
+++ stop.sh
@@ -1,6 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-PORT=$(cat local/port.md | head -n 1 | tr -d '[:space:]')
-
-BASE_DIR=$(cd "$(dirname "$0")" && pwd)
-"$(dirname "$0")/../transit-local-services/scripts/killPorts.sh" $PORT "$BASE_DIR"
+"$(dirname "$0")/../transit-local-services/scripts/stop.sh"
